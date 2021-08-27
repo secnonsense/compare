9e541494dae5bed7827e42ff97341a8487987153
secnonsense <secnonsense>
2021-08-27 18:49:35 -0400

    added -i for case insensitive searches

--- compare.py
+++ compare.py
@@ -2,7 +2,10 @@
 
 import argparse
 
-def compare_string(string1,string2,nomatch=0,i=0,d=0,x=0):
+def compare_string(string1,string2,nomatch=0,i=0,d=0,x=0,ignore=0):
+    if ignore:
+        string1=string1.lower()
+        string2=string2.lower()
     if string1 in string2 and x==0:
         if i==1 and d==0:
                 print(string1)
@@ -18,14 +21,14 @@ def compare_string(string1,string2,nomatch=0,i=0,d=0,x=0):
         toggle=1
         return toggle
 
-def compare_files(file,file2,nomatch=0,i=0,d=0,x=0):
+def compare_files(file,file2,nomatch=0,i=0,d=0,x=0,ignore=0):
     count=0
     with open(file, 'r') as string1_file:
             for string1 in string1_file:
                 with open(file2, 'r') as string2_file:
                     match=0
                     for string2 in string2_file:
-                        toggle=compare_string(string1.strip(),string2.strip(),nomatch,i,d,x)
+                        toggle=compare_string(string1.strip(),string2.strip(),nomatch,i,d,x,ignore)
                         if toggle==1:
                             match=1
                             count+=1
@@ -44,13 +47,14 @@ def parse_args():
     parser.add_argument("-m", "--matchonly", help="Print a list of matching strings only", action="store_true")
     parser.add_argument("-d", "--dontmatch", help="Print a list of strings that don't match", action="store_true")
     parser.add_argument("-x", "--exactmatch", help="Print a list of strings that match exactly", action="store_true")
+    parser.add_argument("-i", "--ignore_case", help="Ignore Case", action="store_true")
     parser.add_argument("source", help="Enter a file with strings to be compared")
     parser.add_argument("dest", help="Enter a file with strings to compare to")
     return parser.parse_args()
 
 def main():
     args=parse_args()
-    compare_files(args.source,args.dest,args.nomatch,args.matchonly,args.dontmatch,args.exactmatch)
+    compare_files(args.source,args.dest,args.nomatch,args.matchonly,args.dontmatch,args.exactmatch,args.ignore_case)
 
 if __name__ == "__main__":
     main()
