4c96a462d9cc806a71284183fc9f234fcb223889
secnonsense <27748027+secnonsense@users.noreply.github.com>
2023-03-07 13:32:38 -0500

    Update compare.py

--- compare.py
+++ compare.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-def compare_string(string1,string2,nomatch=0,i=0,d=0,x=0,ignore=0):
+def compare_string(string1,string2,nomatch=0,i=0,d=0,x=0,ignore=0,matchdest=0):
     string1a=string1
     string2a=string2
     if ignore:
@@ -10,7 +10,9 @@ def compare_string(string1,string2,nomatch=0,i=0,d=0,x=0,ignore=0):
         string2=string2.lower()
     if string1 in string2 and x==0:
         if i==1 and d==0:
-                print(string1a)
+            print(string1a)
+        elif i==1 and d==0 and matchdest=1:
+            print(string2a)
         elif i==0 and d==0:
             print(f"string {string1a} matches {string2a}")
         toggle=1
@@ -23,14 +25,14 @@ def compare_string(string1,string2,nomatch=0,i=0,d=0,x=0,ignore=0):
         toggle=1
         return toggle
 
-def compare_files(file,file2,nomatch=0,i=0,d=0,x=0,ignore=0):
+def compare_files(file,file2,nomatch=0,i=0,d=0,x=0,ignore=0,matchdest=0):
     count=0
     with open(file, 'r') as string1_file:
             for string1 in string1_file:
                 with open(file2, 'r') as string2_file:
                     match=0
                     for string2 in string2_file:
-                        toggle=compare_string(string1.strip(),string2.strip(),nomatch,i,d,x,ignore)
+                        toggle=compare_string(string1.strip(),string2.strip(),nomatch,i,d,x,ignore,matchdest)
                         if toggle==1:
                             match=1
                             count+=1
@@ -49,6 +51,7 @@ def parse_args():
     parser.add_argument("-m", "--matchonly", help="Print a list of matching strings only", action="store_true")
     parser.add_argument("-d", "--dontmatch", help="Print a list of strings that don't match", action="store_true")
     parser.add_argument("-x", "--exactmatch", help="Print a list of strings that match exactly", action="store_true")
+    parser.add_argument("-w", "--matchdest", help="Print a list of what has been matched", action="store_true")
     parser.add_argument("-i", "--ignore_case", help="Ignore Case", action="store_true")
     parser.add_argument("source", help="Enter a file with strings to be compared")
     parser.add_argument("dest", help="Enter a file with strings to compare to")
@@ -56,7 +59,7 @@ def parse_args():
 
 def main():
     args=parse_args()
-    compare_files(args.source,args.dest,args.nomatch,args.matchonly,args.dontmatch,args.exactmatch,args.ignore_case)
+    compare_files(args.source,args.dest,args.nomatch,args.matchonly,args.dontmatch,args.exactmatch,args.ignore_case,args.matchdest)
 
 if __name__ == "__main__":
     main()
